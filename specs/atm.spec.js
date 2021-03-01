e5f5d5c79f3a88dcf435f2b1ca0385bd7b52642b
Thomas <thomas@craftacademy.se>
2021-03-01 20:47:03 +0100

    expanded the withdraw method used sinon to stub out the account object

--- specs/atm.spec.js
+++ specs/atm.spec.js
@@ -1,12 +1,15 @@
 const { expect } = require("chai");
+const sinon = require('sinon')
 
 const Atm = require('../src/atm')
 
 describe('Atm class', () => {
 
-  let subject
+  let subject, account
   before(() => {
     subject = new Atm()
+    account = { pin: null, balance: null }
+    sinon.stub(account, 'pin').value('1234')
   })
 
   it('can be instantiated', () => {
@@ -18,18 +21,24 @@ describe('Atm class', () => {
   });
 
   it('funds are reduced on withdraw', () => {
-    subject.withdraw(50)
+    subject.withdraw(50, account, '1234')
     expect(subject.funds).to.equal(950)
   });
 
   it('dispatches funds in bill denominations', () => {
     const expectedBills = [20, 20, 10]
-    const response = subject.withdraw(50)
+    const response = subject.withdraw(50, account, '1234')
     expect(response.bills).to.eql(expectedBills)
   });
 
   it('is expected to return a success message', () => {
-    let response = subject.withdraw(50)
+    let response = subject.withdraw(50, account, '1234')
     expect(response.status).to.equal('success')
+  });
+
+  it('is expected to reject withdraw attepmt if provided with wrong pin', () => {
+    let response = subject.withdraw(50, account, '0000')
+    expect(response.status).to.equal('error')
+    expect(response.message).to.equal('You entered the wrong pin code, Please try again!')
   });
 });
--- src/atm.js
+++ src/atm.js
@@ -4,16 +4,20 @@ class Atm {
     this.funds = 1000
   }
 
-  withdraw(amount) {
-    this.funds -= amount
-    let billsToDispatch = []
-    this.carriedDenominations.forEach(denomination => {
-      while (amount - denomination >= 0) {
-        amount -= denomination
-        billsToDispatch.push(denomination)
-      }
-    });
-    return { status: 'success', bills: billsToDispatch }
+  withdraw(amount, account, pin) {
+    if (account.pin === pin ) {
+      this.funds -= amount
+      let billsToDispatch = []
+      this.carriedDenominations.forEach(denomination => {
+        while (amount - denomination >= 0) {
+          amount -= denomination
+          billsToDispatch.push(denomination)
+        }
+      });
+      return { status: 'success', bills: billsToDispatch }
+    } else {
+      return { status: 'error', message: "You entered the wrong pin code, Please try again!" }
+    }
   }
 
 }
